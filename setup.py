67283b035e5897f1778856ac68922efa9f8d91a6
ammonthompson <ammonthompson@gmail.com>
2025-01-03 16:42:29 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="phylo_autoencode",  # Package name
+    name="phyloencode",  # Package name
     version="0.1",      # Package version
     packages=find_packages(),  # Automatically find submodules
     install_requires=[],       # Dependencies (if any)
