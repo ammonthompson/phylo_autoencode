1a8465815b1e5864015d9a53733c540213601a71
ammonthompson <ammonthompson@gmail.com>
2025-01-08 12:32:02 -0500

    make AEData class

--- phyloencode/DataProcessors.py
+++ phyloencode/DataProcessors.py
@@ -3,7 +3,88 @@ import torch
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 import h5py
 import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from typing import List, Dict, Tuple, Optional, Union
+
+# class that contains the DataSet and DataLoaders
+# splits and normalizes/denormalizes it
+# 
+# Parameters: 
+# (phy data, aux data) -> Tuple[torch.Tensor, torch.Tensor], 
+# p: proportion of data fro training -> float
+# nc: number of channels in the data for reshapeing phy data -> int
+# 
+class AEData(object):
+    def __init__(self, data: Tuple[torch.Tensor, torch.Tensor],
+                 prop_train: float, nchannels: int):
+        """
+        each tree in data is assumed to be flattend in column-major order
+        of a matrix of dimensions (nchannels, ntips)
+        """
+        self.nchannels = nchannels
+        self.phy_data = data[0]
+        self.aux_data = data[1]
+        self.data = np.hstack((self.phy_data, self.aux_data))
+        phy_width = self.phy_data.shape[1]
+
+        # split data 
+        num_train = int(prop_train * self.phy_data.shape[0])
+        train_data, val_data = train_test_split(self.data, train_size = num_train, shuffle=True)
+        train_phy_data = train_data[:,:phy_width]
+        train_aux_data = train_data[:,phy_width:]
+        val_phy_data   = val_data[:,:phy_width]
+        val_aux_data   = val_data[:,phy_width:]
+
+        # standardize train data
+        self.phy_ss = StandardScaler()
+        self.aux_ss = StandardScaler()
+        self.phy_normalizer = self.phy_ss.fit(train_phy_data)
+        self.aux_normalizer = self.aux_ss.fit(train_aux_data)
+        self.norm_train_phy_data = self.phy_normalizer.transform(train_phy_data)
+        self.norm_train_aux_data = self.aux_normalizer.transform(train_aux_data)
+        self.norm_val_phy_data   = self.phy_normalizer.transform(val_phy_data)
+        self.norm_val_aux_data   = self.aux_normalizer.transform(val_aux_data)
+
+        # reshape phy data to (num examples, num channels, num tips)
+        # (num examples, num channels x num tips) -> (num examples, num channels, num tips)
+        assert(train_phy_data.shape[1] % nchannels == 0)
+        self.norm_train_phy_data = self.norm_train_phy_data.reshape((self.norm_train_phy_data.shape[0], 
+                                                        nchannels, 
+                                                        int(self.norm_train_phy_data.shape[1]/nchannels)),
+                                                        order = "F")
+        self.norm_val_phy_data   = self.norm_val_phy_data.reshape((self.norm_val_phy_data.shape[0], 
+                                                        nchannels, 
+                                                        int(self.norm_val_phy_data.shape[1]/nchannels)),
+                                                        order = "F")
+        self.phy_width = self.norm_train_phy_data.shape[2]
+        self.aux_width = self.norm_train_aux_data.shape[1]
+
+        # create Datasets. __getitem__() returns a tuple (phy, aux)
+        self.train_dataset = TreeDataSet(self.norm_train_phy_data, self.norm_train_aux_data)
+        self.val_dataset   = TreeDataSet(self.norm_val_phy_data,   self.norm_val_aux_data)
+
+    def get_datasets(self) -> Tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+        return self.train_dataset, self.val_dataset
+
+    def get_normalizers(self) -> Tuple[sklearn.preprocessing.StandardScaler, sklearn.preprocessing.StandardScaler]:
+        return self.phy_normalizer, self.aux_normalizer
+    
+    def get_dataloaders(self, 
+                        batch_size = 32, 
+                        shuffle = True, 
+                        num_workers = 0) -> Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
+        # data loaders
+        self.train_dataloader = DataLoader(self.train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=shuffle, 
+                                           num_workers=num_workers)
+        self.val_dataloader   = DataLoader(self.val_dataset)
+        return self.train_dataloader, self.val_dataloader
+
+
+
 
 # these classes work with datasets output from the Format step in Phyddle
 class TreeDataSet(Dataset):
--- sandbox/tester.py
+++ sandbox/tester.py
@@ -24,66 +24,79 @@ with h5py.File("test_data/peak_time.train.hdf5", "r") as f:
     test_aux_data = torch.tensor(f['aux_data'][45000:45100,...], dtype = torch.float32)
 
 
-phy_width = phy_data.shape[1]
-aux_width = aux_data.shape[1]
+# phy_width = phy_data.shape[1]
+# aux_width = aux_data.shape[1]
 
-num_train = int(0.8 * phy_data.shape[0])
+# num_train = int(0.8 * phy_data.shape[0])
 
-data = np.hstack((phy_data, aux_data))
+# data = np.hstack((phy_data, aux_data))
 
-# split data 
-train_data, val_data = train_test_split(data, train_size = num_train, shuffle=True)
-train_phy_data = train_data[:,:phy_width]
-train_aux_data = train_data[:,phy_width:]
-val_phy_data   = val_data[:,:phy_width]
-val_aux_data   = val_data[:,phy_width:]
+# # split data 
+# train_data, val_data = train_test_split(data, train_size = num_train, shuffle=True)
+# train_phy_data = train_data[:,:phy_width]
+# train_aux_data = train_data[:,phy_width:]
+# val_phy_data   = val_data[:,:phy_width]
+# val_aux_data   = val_data[:,phy_width:]
 
 
-# standardize train data
-phy_ss = StandardScaler()
-aux_ss = StandardScaler()
-phy_normalizer = phy_ss.fit(train_phy_data)
-aux_normalizer = aux_ss.fit(train_aux_data)
-norm_train_phy_data = phy_normalizer.transform(train_phy_data)
-norm_train_aux_data = aux_normalizer.transform(train_aux_data)
-norm_val_phy_data   = phy_normalizer.transform(val_phy_data)
-norm_val_aux_data   = aux_normalizer.transform(val_aux_data)
+# # standardize train data
+# phy_ss = StandardScaler()
+# aux_ss = StandardScaler()
+# phy_normalizer = phy_ss.fit(train_phy_data)
+# aux_normalizer = aux_ss.fit(train_aux_data)
+# norm_train_phy_data = phy_normalizer.transform(train_phy_data)
+# norm_train_aux_data = aux_normalizer.transform(train_aux_data)
+# norm_val_phy_data   = phy_normalizer.transform(val_phy_data)
+# norm_val_aux_data   = aux_normalizer.transform(val_aux_data)
 
-# reshape phy data (in this case channels are 3 locs and 4 cblv dimensions)
-nchannels = 7
-assert(train_phy_data.shape[1] % nchannels == 0)
-norm_train_phy_data = norm_train_phy_data.reshape((norm_train_phy_data.shape[0], 
-                                                   nchannels, 
-                                                   int(norm_train_phy_data.shape[1]/nchannels)),
-                                                   order = "F")
-norm_val_phy_data   = norm_val_phy_data.reshape((norm_val_phy_data.shape[0], 
-                                                 nchannels, 
-                                                 int(norm_val_phy_data.shape[1]/nchannels)),
-                                                 order = "F")
-phy_width = norm_train_phy_data.shape[2]
-aux_width = norm_train_aux_data.shape[1]
+# # reshape phy data (in this case channels are 3 locs and 4 cblv dimensions)
+# nchannels = 7
+# assert(train_phy_data.shape[1] % nchannels == 0)
+# norm_train_phy_data = norm_train_phy_data.reshape((norm_train_phy_data.shape[0], 
+#                                                    nchannels, 
+#                                                    int(norm_train_phy_data.shape[1]/nchannels)),
+#                                                    order = "F")
+# norm_val_phy_data   = norm_val_phy_data.reshape((norm_val_phy_data.shape[0], 
+#                                                  nchannels, 
+#                                                  int(norm_val_phy_data.shape[1]/nchannels)),
+#                                                  order = "F")
+# phy_width = norm_train_phy_data.shape[2]
+# aux_width = norm_train_aux_data.shape[1]
 
 
 # create Datasets. __getitem__() returns a tuple (phy, aux)
-train_dataset = dataproc.TreeDataSet(norm_train_phy_data, norm_train_aux_data)
-val_dataset   = dataproc.TreeDataSet(norm_val_phy_data,   norm_val_aux_data)
+# train_dataset = dataproc.TreeDataSet(norm_train_phy_data, norm_train_aux_data)
+# val_dataset   = dataproc.TreeDataSet(norm_val_phy_data,   norm_val_aux_data)
+
+
+# create data object
+ae_data = dataproc.AEData( data       = (phy_data, aux_data),
+                           prop_train = 0.8, 
+                           nchannels  = 7)
+
 
 # data loaders
-train_dataloader = td.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=0)
-val_dataloader   = td.DataLoader(val_dataset)
+# train_dataloader = td.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=0)
+# val_dataloader   = td.DataLoader(val_dataset)
+train_dataloader, val_dataloader = ae_data.get_dataloaders(batch_size=32, 
+                                                           shuffle=True, 
+                                                           num_workers=0)
 
 # create model
-ae_model  = aem.PhyloAEModelCNN(nchannels, phy_width, aux_width, 
-                                stride = [2,4,4,8],
-                                kernel = [3,5,5,9],
-                                out_channels=[8,12,14,16])
+ae_model  = aem.PhyloAEModelCNN(ae_data.nchannels, 
+                                ae_data.phy_width, 
+                                ae_data.aux_width, 
+                                stride = [4,4,8],
+                                kernel = [5,5,9],
+                                out_channels=[24,24,32])
 loss_fx   = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(ae_model.parameters())
 
 # Train
+rand_seed = np.random.randint(0,10000)
 tree_autoencoder = pa.PhyloAutoencoder(model=ae_model, optimizer=optimizer, loss_func=loss_fx, phy_loss_weight=1.0)
 tree_autoencoder.set_data_loaders(train_loader=train_dataloader, val_loader=val_dataloader)
-tree_autoencoder.train(num_epochs = 20, seed = 1)
+tree_autoencoder.train(num_epochs = 10, seed = rand_seed)
 # tree_autoencoder.plot_losses()
 
 print("Training Finished")
@@ -91,9 +104,12 @@ print("Training Finished")
 
 
 # Test data
+# phydat   = phy_normalizer.transform(test_phy_data)
+# auxdat   = aux_normalizer.transform(test_aux_data)
+phy_normalizer, aux_normalizer = ae_data.get_normalizers()
 phydat   = phy_normalizer.transform(test_phy_data)
 auxdat   = aux_normalizer.transform(test_aux_data)
-phydat = phydat.reshape((phydat.shape[0], nchannels, int(phydat.shape[1]/nchannels)), order = "F")
+phydat = phydat.reshape((phydat.shape[0], ae_data.nchannels, int(phydat.shape[1]/ae_data.nchannels)), order = "F")
 phydat = torch.Tensor(phydat)
 auxdat = torch.Tensor(auxdat)
 phy_pred, auxpred = tree_autoencoder.predict(phydat, auxdat)
