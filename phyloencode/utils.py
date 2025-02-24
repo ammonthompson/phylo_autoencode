2b86417077ed1c384b48b95785284f331b672914
ammonthompson <ammonthompson@gmail.com>
2025-02-24 14:10:55 -0500

    add maximum mean discrepancy loss to utils

--- phyloencode/utils.py
+++ phyloencode/utils.py
@@ -33,10 +33,61 @@ def get_vae_loss_function():
 
         # kl_loss = (0.5 * ((x - mu) ** 2 / var + log_var + torch.log(2 * torch.pi))).mean()
         
-        return 0*decoded_loss, kl_loss
+        return decoded_loss, kl_loss
         
 
     return vae_loss_fx
+
+
+def get_mmd_loss_function():
+
+    def get_mmd(pred, true, latent_pred, y, mmd_weight = 0,):
+        """Emprical maximum mean discrepancy. The lower the result
+        the more evidence that distributions are the same.
+        Derived From: 
+            https://www.onurtunali.com/ml/2019/03/08/maximum-mean-discrepancy-in-machine-learning.html#references
+        MMD^2 = E[k(xx, xx)] + E[k(yy, yy)] - 2E[k(xx,yy)]
+
+        Args:
+            pred: decoded data
+            true: true data
+            latent_pred: first sample, distribution P
+            kernel: kernel type such as "multiscale" or "rbf"
+        """
+
+        recon_loss = torch.nn.MSELoss(reduction = "mean")(pred, true)
+        x = latent_pred
+        y = torch.randn(x.shape)
+
+        xx, yy, xy = torch.mm(x, x.t()), torch.mm(y, y.t()), torch.mm(x, y.t())
+        # create a matrix with columns set to repeats of each diagonal element
+        # [[1,2]  -> [[1,4]
+        #  [3,4]] ->  [1,4]]
+        rx = (xx.diag().unsqueeze(0).expand_as(xx)) 
+        ry = (yy.diag().unsqueeze(0).expand_as(yy))
+
+        # ||x - x'||^2
+        dxx = rx.t() + rx - 2. * xx # Used for A in (1)
+        dyy = ry.t() + ry - 2. * yy # Used for B in (1)
+        dxy = rx.t() + ry - 2. * xy # Used for C in (1)
+
+        device = latent_pred.device
+        XX, YY, XY = (torch.zeros(xx.shape).to(device),
+                      torch.zeros(xx.shape).to(device),
+                      torch.zeros(xx.shape).to(device))
+
+
+        bandwidth_range = [10, 15, 20, 50]
+        for a in bandwidth_range:
+            XX += torch.exp(-0.5*dxx/a)
+            YY += torch.exp(-0.5*dyy/a)
+            XY += torch.exp(-0.5*dxy/a)
+
+        MMD2 = torch.mean(XX + YY - 2. * XY)
+                
+        return recon_loss, mmd_weight * MMD2
+
+    return get_mmd
 
 
 def conv1d_sequential_outshape(sequential: nn.Sequential, 
