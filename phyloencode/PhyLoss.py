fix masking. tidy up

phyloencode/PhyLoss.py
@@ -6,6 +6,7 @@ import torch.nn.functional as fun
 from typing import List, Dict, Tuple, Optional, Union
 import numpy as np
 import itertools
+import matplotlib.pyplot as plt
 
 
 
@@ -157,11 +158,11 @@ class PhyLoss(object):
 
     def print_epoch_losses(self, elapsed_time):
         if self.validation:
-            loss_type = "Train loss: "
+            loss_type = "Val loss:   "
         else:
             print(f"Epoch {len(self.epoch_total_loss)}")
-            loss_type = "Val loss:   "
-            
+            loss_type = "Train loss: "
+
         print(  f"\t {loss_type}{self.epoch_total_loss[-1]:.4f},  " +
                 f"phy L: {self.epoch_phy_loss[-1]:.4f},  " +
                 f"char L: {self.epoch_char_loss[-1]:.4f},  " +
@@ -202,49 +203,57 @@ class PhyLoss(object):
         """
         # if cblv-like data, use the first two channels in the loss
         # else if augmented cblv-like data, use the first four channels in the data
+        # TODO: instead of computing mask.sum() you can pass in num_tips 
         if mask is None:
-            tree_loss = fun.mse_loss(x, y) 
+            batch_mean_tree_loss = fun.mse_loss(x, y, reduction = "mean") 
         else:
-            tree_loss = (fun.mse_loss(x, y, reduction='none') * mask).mean()  
+            tree_mse = ((fun.mse_loss(x, y, reduction='none') * mask).sum(dim=(1,2)) / 
+                         mask.sum(dim = (1,2))) 
+            batch_mean_tree_loss = tree_mse.mean()
 
         tip1_loss = 0.1 * fun.mse_loss(x[:,0:2,0], y[:,0:2,0]) 
 
-        return tree_loss + tip1_loss
+        return batch_mean_tree_loss + tip1_loss
 
     def _char_recon_loss(self, x, y, char_type = "categorical", mask = None):
         """
         Compute the reconstruction loss for categorical character data.
 
         Args:
-            x (torch.Tensor): Reconstructed character data ((nchannels - 2) x ntips or 
-                                (nchannels - 4) x ntips).
-            y (torch.Tensor): Original character data ((nchannels - 2) x ntips or 
-                                (nchannels - 4) x ntips).
+        # TODO: fix this doc string (num_chars)
+            x (torch.Tensor): Reconstructed character data.
+            y (torch.Tensor): Original character data.
             is_categorical (bool): Whether the character data is categorical.
+            mask : tips that are just padding
 
         Returns:
             torch.Tensor: Reconstruction loss for character data.
         """
+        # TODO: instead of computing mask.sum() you can pass in num_tips 
 
         if char_type == "categorical": 
-            # Use cross-entropy loss for categorical data
-            # Reshape to (batch_size * ntips, n_classes) for cross-entropy
-            x = x.permute(0, 2, 1).reshape(-1, x.shape[1])  # (batch_size * ntips, n_classes)
-            y = y.permute(0, 2, 1).reshape(-1, y.shape[1])  # (batch_size * ntips, n_classes)
-
             # Convert one-hot to class indices
-            y = y.argmax(dim=1)  # shape (N,)
+            # y = y.argmax(dim=1)  # shape (N, maxtips)
             
-            char_loss = fun.cross_entropy(x, y, reduction = 'none')
+            char_loss = fun.cross_entropy(x, y, reduction = 'none') 
         else:
             char_loss = fun.mse_loss(x, y, reduction = 'none')
 
-        if mask is not None:
-            # Apply the mask to the loss
-            mask = mask.permute(0, 2, 1).reshape(-1, mask.shape[1])
-            char_loss = char_loss * mask[:,0] # match dims (all columns are the same in mask)
+        # print(x.shape, y.shape, mask.shape)
+        # print(char_loss.shape)
+        # print("x: ", x[0,:,0:5])
+        # print("y: ", y[0,0:5])
+        # print("char_loss: ", char_loss[0,0:5])
+        # print("------------")
 
-        return char_loss.mean()
+        # quit()
+
+        if mask is not None:
+            pb_char_loss = (char_loss * mask[:,0,:] ).sum(dim=1) / mask[:,0,:].sum(dim=1)# match dims (all columns are the same in mask)
+        else:
+            pb_char_loss = char_loss
+
+        return pb_char_loss.mean()
 
     def _aux_recon_loss(self, x, y):
         """
@@ -268,10 +277,9 @@ class PhyLoss(object):
 
     def _mmd_loss(self, latent_pred, target = None):
 
-        device = latent_pred.device
         x = latent_pred
         if target is None:
-            y = torch.randn(x.shape, requires_grad=False).to(device)
+            y = torch.randn(x.shape, requires_grad=False).to(x.device)
         else:
             y = target[0:x.shape[0],:]
         # MMD = MMDLoss(device)(x, y)
@@ -290,6 +298,9 @@ class PhyLoss(object):
         VZ = self.vz(x,y)
 
         return VZ
+
+
+
 
     # Experimental
     def _rdp_loss(self, Z_batch: torch.Tensor, X_batch: torch.Tensor, rand_matrix: torch.Tensor, n_pairs: int):
@@ -437,6 +448,7 @@ class VZLoss(nn.Module):
     def forward(self, X, Y):
         return self.kernel(X.T, Y.T).var()
     
+
 
 
 # storage

phyloencode/PhyloAEModel.py
@@ -181,69 +181,6 @@ class AECNN(nn.Module):
         self.write_network_to_file(out_prefix + ".network.txt")
         
  
-    def old_forward(self, data: Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
-        """        
-        Push data through the encoder and decoder networks.
-
-        Args:
-            data (Tuple[torch.Tensor, torch.Tensor]): _description_
-
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor]: _description_
-        """
-        
-
-        # data is a tuple (structured, unstructured)
-
-        # Encode
-        structured_encoded_x   = self.structured_encoder(data[0]) # (nbatch, nchannels, out_width)
-        unstructured_encoded_x = self.unstructured_encoder(data[1]) # (nbatch, out_width)
-
-        # TODO: THe latent Classes should be able to handle the reshaping of the data
-        # Combine encodings and reshape depending on the latent layer type
-        flat_structured_encoded_x = structured_encoded_x.flatten(start_dim=1)
-        combined_latent           = torch.cat((flat_structured_encoded_x, unstructured_encoded_x), dim=1)  # aux latent last
-
-        if self.latent_layer_type == "CNN":
-            reshaped_shared_latent = combined_latent.view(-1, self.num_structured_latent_channels, 
-                                                              self.reshaped_shared_latent_width)
-            shared_latent_out      = self.latent_layer(reshaped_shared_latent)
-            structured_decoded_x   = self.structured_decoder(shared_latent_out)
-            unstructured_decoded_x = self.unstructured_decoder(shared_latent_out.flatten(start_dim=1))
-        
-        elif self.latent_layer_type == "GAUSS":
-            shared_latent_out   = self.latent_layer(combined_latent)
-
-            # testings
-            decoded_shared_latent_out = self.latent_layer_decoder(shared_latent_out)
-
-            reshaped_decoded_shared_latent_out = decoded_shared_latent_out.view(-1, self.num_structured_latent_channels, 
-                                                                                    self.reshaped_shared_latent_width) 
-            structured_decoded_x   = self.structured_decoder(reshaped_decoded_shared_latent_out)
-            unstructured_decoded_x = self.unstructured_decoder(decoded_shared_latent_out)
-        
-        elif self.latent_layer_type == "DENSE":
-            shared_latent_out   = self.latent_layer(combined_latent)            
-            reshaped_latent_out = shared_latent_out.view(-1, self.num_structured_latent_channels, 
-                                                                self.reshaped_shared_latent_width)
-            structured_decoded_x   = self.structured_decoder(reshaped_latent_out)
-            unstructured_decoded_x = self.unstructured_decoder(shared_latent_out)
-
-        # separate the two type of structured decoded: tree and character data
-        # maybe this should be done somewhere more downstream
-        if self.num_chars > 0:
-            phy_decoded_x = structured_decoded_x[:,:(structured_decoded_x.shape[1]-self.num_chars),:]
-            char_decoded_x = structured_decoded_x[:,(structured_decoded_x.shape[1]-self.num_chars):,:]
-        else:
-            phy_decoded_x = structured_decoded_x
-            char_decoded_x = None
-        # model should output the latent layer if layer type is "GAUSS"
-        if self.latent_layer_type != "GAUSS":
-            shared_latent_out = None
-
-        return phy_decoded_x, char_decoded_x, unstructured_decoded_x, shared_latent_out
-
-
     def forward(self, data: Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
         """        
         Push data through the encoder and decoder networks.
@@ -259,6 +196,7 @@ class AECNN(nn.Module):
 
         # Encode/Decode
         shared_latent_out        = self.encode(data[0], data[1])
+
         (structured_decoded_x, 
          unstructured_decoded_x) = self.decode(shared_latent_out)
 
@@ -323,7 +261,7 @@ class AECNN(nn.Module):
             self.train(is_training)
     
     
-    def decode(self, z: torch.Tensor, *, num_tips = None,
+    def decode(self, z: torch.Tensor, *, num_tips_auxidx : int = None,
                inference = False, detach = False) -> Tuple[torch.Tensor, torch.Tensor]:
         
         is_training = self.training
@@ -348,6 +286,11 @@ class AECNN(nn.Module):
                     char_start_idx = decoded_tree.shape[1] - self.num_chars
                     decoded_tree[:,char_start_idx:,:] = torch.softmax(decoded_tree[:,char_start_idx:,:], dim = 1)
 
+                # if num_tips provided, set all indices > column idx implied by num_tips to zero
+                if num_tips_auxidx is not None:
+                    idx = num_tips_auxidx
+                    decoded_tree = utils.set_pred_pad_to_zero(decoded_tree, decoded_aux[:,idx])
+
                 if inference and detach:
                     decoded_tree = decoded_tree.detach()
                     decoded_aux  = decoded_aux.detach()
@@ -355,8 +298,8 @@ class AECNN(nn.Module):
         finally:
             self.train(is_training)
 
-    # TODO: should prob output torch.Tensors
-    def predict(self, phy: torch.Tensor, aux: torch.Tensor,
+    # TODO: should prob output torch.Tensors like everything else?
+    def predict(self, phy: torch.Tensor, aux: torch.Tensor, *, num_tips_auxidx : int  = None,
                 inference = False, detach = False) -> Tuple[np.ndarray, np.ndarray]:
                 # pushes data through full autoencoder
         # TODO: maybe should use num tips prediction to set implied predicted padding area to zero
@@ -371,22 +314,22 @@ class AECNN(nn.Module):
             with grad_context:
                 phy = phy.to(self.device)
                 aux = aux.to(self.device)
-                tree_pred, char_pred, aux_pred, latent = self((phy, aux))
+                tree_pred, char_pred, aux_pred, _ = self((phy, aux))
 
                 if char_pred is not None:
-                    if self.char_type == "categorical":
-                        char_sftmx = torch.softmax(char_pred, dim = 1)
-                        phy_pred = torch.cat((tree_pred, char_sftmx), dim = 1)
-                    else:
-                        phy_pred =  torch.cat((tree_pred, char_pred), dim = 1)
+                    phy_pred =  torch.cat((tree_pred, char_pred), dim = 1)
                 else:
                     phy_pred = tree_pred
+
+                if num_tips_auxidx is not None:
+                    idx = num_tips_auxidx
+                    decoded_tree = utils.set_pred_pad_to_zero(decoded_tree, aux_pred[:,idx])
 
                 if inference and detach:
                     phy_pred = phy_pred.detach()
                     aux_pred = aux_pred.detach()
 
-                return phy_pred.detach().cpu().numpy(), aux_pred.detach().cpu().numpy()
+                return phy_pred.cpu().numpy(), aux_pred.cpu().numpy()
         finally:
             self.train(is_training)
 
@@ -408,6 +351,71 @@ class AECNN(nn.Module):
             f.write(str(self.unstructured_encoder) + "\n")
             f.write("\nSee latent layers above.\n")
             f.write(str(self.unstructured_decoder))
+
+    def old_forward(self, data: Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
+        """        
+        Push data through the encoder and decoder networks.
+
+        Args:
+            data (Tuple[torch.Tensor, torch.Tensor]): _description_
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: _description_
+        """
+        
+
+        # data is a tuple (structured, unstructured)
+
+        # Encode
+        structured_encoded_x   = self.structured_encoder(data[0]) # (nbatch, nchannels, out_width)
+        unstructured_encoded_x = self.unstructured_encoder(data[1]) # (nbatch, out_width)
+
+        # TODO: THe latent Classes should be able to handle the reshaping of the data
+        # Combine encodings and reshape depending on the latent layer type
+        flat_structured_encoded_x = structured_encoded_x.flatten(start_dim=1)
+        combined_latent           = torch.cat((flat_structured_encoded_x, unstructured_encoded_x), dim=1)  # aux latent last
+
+        if self.latent_layer_type == "CNN":
+            reshaped_shared_latent = combined_latent.view(-1, self.num_structured_latent_channels, 
+                                                              self.reshaped_shared_latent_width)
+            shared_latent_out      = self.latent_layer(reshaped_shared_latent)
+            structured_decoded_x   = self.structured_decoder(shared_latent_out)
+            unstructured_decoded_x = self.unstructured_decoder(shared_latent_out.flatten(start_dim=1))
+        
+        elif self.latent_layer_type == "GAUSS":
+            shared_latent_out   = self.latent_layer(combined_latent)
+
+            # testings
+            decoded_shared_latent_out = self.latent_layer_decoder(shared_latent_out)
+
+            reshaped_decoded_shared_latent_out = decoded_shared_latent_out.view(-1, self.num_structured_latent_channels, 
+                                                                                    self.reshaped_shared_latent_width) 
+            structured_decoded_x   = self.structured_decoder(reshaped_decoded_shared_latent_out)
+            unstructured_decoded_x = self.unstructured_decoder(decoded_shared_latent_out)
+        
+        elif self.latent_layer_type == "DENSE":
+            shared_latent_out   = self.latent_layer(combined_latent)            
+            reshaped_latent_out = shared_latent_out.view(-1, self.num_structured_latent_channels, 
+                                                                self.reshaped_shared_latent_width)
+            structured_decoded_x   = self.structured_decoder(reshaped_latent_out)
+            unstructured_decoded_x = self.unstructured_decoder(shared_latent_out)
+
+        # separate the two type of structured decoded: tree and character data
+        # maybe this should be done somewhere more downstream
+        if self.num_chars > 0:
+            phy_decoded_x = structured_decoded_x[:,:(structured_decoded_x.shape[1]-self.num_chars),:]
+            char_decoded_x = structured_decoded_x[:,(structured_decoded_x.shape[1]-self.num_chars):,:]
+        else:
+            phy_decoded_x = structured_decoded_x
+            char_decoded_x = None
+        # model should output the latent layer if layer type is "GAUSS"
+        if self.latent_layer_type != "GAUSS":
+            shared_latent_out = None
+
+        return phy_decoded_x, char_decoded_x, unstructured_decoded_x, shared_latent_out
+
+
+
 
 # encoder classes
 class DenseEncoder(nn.Module):

phyloencode/PhyloAutoencoder.py
@@ -8,10 +8,12 @@ import torch
 # from sklearn.preprocessing import StandardScaler
 # from sklearn.model_selection import train_test_split
 # from phyloencode import utils
-from phyloencode.PhyLoss import PhyLoss
+# from phyloencode.PhyLoss import PhyLoss
+from phyloencode.DataProcessors import AEData
 import time
 # import os
 from typing import List, Dict, Tuple, Optional, Union
+
 
 class PhyloAutoencoder(object):
     def __init__(self, model, optimizer, 
@@ -30,7 +32,6 @@ class PhyloAutoencoder(object):
         """
         
         # TODO: define the model object better (autoencoder ...)
-        # TODO: the Loss object should maybe be passed in as a parameter
         # TODO: run checks that the model has the expected attributes
         # TODO: Update seed functionality
         # TODO: create an aux_weight, change everything to "lambda" or "weight" for consistency
@@ -55,15 +56,6 @@ class PhyloAutoencoder(object):
         self.num_tree_chans     = self.phy_channels - self.nchars
         self.latent_shape       = (self.batch_size, self.model.latent_outwidth)
 
-        # TODO: move weights and loss objects out of the class. self.weights is only used by PhyLoss
-        # and Phyloss can be handled by a single object.
-        # self.char_weight        = char_weight if self.nchars > 0 else torch.tensor(0.)
-        # self.weights = (phy_loss_weight, self.char_weight, 1-phy_loss_weight, mmd_lambda, vz_lambda)
-
-        # self.train_loss  = PhyLoss(self.weights, self.aux_ntax_cidx, self.char_type,
-        #                            self.model.latent_layer_type, self.device)
-        # self.val_loss    = PhyLoss(self.weights, self.aux_ntax_cidx, self.char_type,
-        #                            self.model.latent_layer_type, self.device)
         self.train_loss = train_loss
         self.val_loss   = val_loss
 
@@ -102,8 +94,7 @@ class PhyloAutoencoder(object):
                 # print epoch mean component losses to screen       
                 self.val_loss.print_epoch_losses(elapsed_time = time.time() - epoch_time)
 
-
-        
+       
     def _mini_batch(self, validation = False):
         """Performs a mini batch step for the model.
         loops through the data loader and performs a step for each batch
@@ -208,25 +199,10 @@ class PhyloAutoencoder(object):
         self.val_loss.minibatch_loss(pred, true, segmented_mask)
 
         
-    def predict(self, phy: torch.Tensor, aux: torch.Tensor) -> Tuple[np.ndarray, np.ndarray]:
-        # pushes data through full autoencoder
-        # TODO: maybe should use num tips prediction to set implied predicted padding area to zero
-        self.model.eval() 
-        phy = phy.to(self.device)
-        aux = aux.to(self.device)
-        tree_pred, char_pred, aux_pred, latent = self.model((phy, aux))
+    def predict(self, phy: torch.Tensor, aux: torch.Tensor, *,
+                inference = True, detach = True) -> Tuple[np.ndarray, np.ndarray]:
 
-        if char_pred is not None:
-            if self.char_type == "categorical":
-                char_sftmx = torch.softmax(char_pred, dim = 1)
-                phy_pred = torch.cat((tree_pred, char_sftmx), dim = 1)
-            else:
-                phy_pred =  torch.cat((tree_pred, char_pred), dim = 1)
-        else:
-            phy_pred = tree_pred
-
-        self.model.train()
-        return phy_pred.detach().cpu().numpy(), aux_pred.detach().cpu().numpy()
+        return self.model.predict(phy, aux, inference = inference, detach = detach)
 
 
     def to_device(self, device):
@@ -236,17 +212,21 @@ class PhyloAutoencoder(object):
         except RuntimeError:
             print(f"Didn't work, sending to {self.device} instead.")
 
-    # TODO: should mask_loader be split to train and val loaders?
     def set_data_loaders(self, train_loader : torch.utils.data.DataLoader, 
                                val_loader   : torch.utils.data.DataLoader = None,
                                mask_loader  : torch.utils.data.DataLoader = None):
         self.train_loader = train_loader
         self.val_loader   = val_loader
-        self.mask_loader  = mask_loader
+        # self.mask_loader  = mask_loader
 
     def set_losses(self, train_loss, val_loss):
         self.train_loss = train_loss,
         self.val_loader = val_loss
+
+    def set_data(self, data : AEData, num_workers : int):
+        self.train_loader, self.val_loader = data.get_dataloaders(self.batch_size, shuffle = True, 
+                                                                    num_workers = num_workers)
+        # self.mask_loader  = mask_loader ???
 
     def _record_grad_norm(self):
         with torch.no_grad():
@@ -255,7 +235,6 @@ class PhyloAutoencoder(object):
                 gn = g.grad.norm().item()
                 self.batch_layer_grad_norm[layer_name].append(gn)
 
-    # TODO: many of these methods should probably be handled by the model object
     def make_graph(self):
         if self.train_loader and self.writer:
             x_sample, y_sample = next(iter(self.train_loader))
@@ -287,58 +266,21 @@ class PhyloAutoencoder(object):
     def save_model(self, filename):
         torch.save(self.model, filename)
 
-    def tree_encode(self, phy: torch.Tensor, aux: torch.Tensor):
-        self.model.eval() 
-        phy = phy.to(self.device)
-        aux = aux.to(self.device)
+    def tree_encode(self, phy: torch.Tensor, aux: torch.Tensor, *,
+               inference = True, detach = True ):
 
-        # get latent unstrcutured and structured embeddings
-        structured_encoded_x   = self.model.structured_encoder(phy)    # (1, nchannels, out_width)
-        unstructured_encoded_x = self.model.unstructured_encoder(aux)  # (1, out_width)
+        return self.model.encode(phy, aux, inference = inference, detach = detach)
 
-        # reshape structured embeddings
-        flat_structured_encoded_x = structured_encoded_x.flatten(start_dim=1)
-        combined_latent           = torch.cat((flat_structured_encoded_x, 
-                                               unstructured_encoded_x), dim=1)
-        
-        
-        # get combined latent output
-        if self.model.latent_layer_type   == "CNN":
-            reshaped_shared_latent = combined_latent.view(-1, self.model.num_structured_latent_channels, 
-                                                              self.model.reshaped_shared_latent_width)
-            shared_latent_out = self.model.latent_layer(reshaped_shared_latent)
+    def latent_decode(self, encoded_tree: torch.Tensor, *, num_tips = None,
+               inference = True, detach = True):
 
-        elif self.model.latent_layer_type == "GAUSS":
-            shared_latent_out = self.model.latent_layer(combined_latent)
-
-        elif self.model.latent_layer_type == "DENSE":
-            shared_latent_out = self.model.latent_layer(combined_latent)
-
-        self.model.train()
-
-        return(shared_latent_out.flatten(start_dim=1))
+        return self.model.decode(encoded_tree, num_tips = num_tips,
+                                 inference = inference, detach = detach)
 
     def get_latent_shape(self):
         return self.model.num_structured_latent_channels, self.model.reshaped_shared_latent_width
-
-    def latent_decode(self, encoded_tree: torch.Tensor):
-        # TODO: maybe should use num tips prediction to set implied predicted padding area to zero
-
-        self.model.eval()
-        encoded_tree = encoded_tree.to(self.device)
-        decoded_latent_layer = self.model.latent_layer_decoder(encoded_tree)
-        # reshape
-        reshaped_decoded_latent_layer = decoded_latent_layer.view(-1, self.model.num_structured_latent_channels, 
-                                                                        self.model.reshaped_shared_latent_width) 
-        decoded_tree = self.model.structured_decoder(reshaped_decoded_latent_layer)
-        decoded_aux  = self.model.unstructured_decoder(decoded_latent_layer.flatten(start_dim=1))
-        if self.char_type == "categorical":
-            # softmax the char data
-            char_start_idx = decoded_tree.shape[1] - self.nchars
-            decoded_tree[:,char_start_idx:,:] = torch.softmax(decoded_tree[:,char_start_idx:,:], dim = 1)
-        self.model.train()
-        return decoded_tree, decoded_aux
-        
+       
+    # TODO: abstract this function. PhyLoss should handle the particulars
     def plot_losses(self, out_prefix = "AElossplot", log = True, starting_epoch = 10):
         # plot total losses
         fig = plt.figure(figsize=(11, 8))
@@ -391,9 +333,9 @@ class PhyloAutoencoder(object):
         
     def _split_tree_char(self, phy : torch.Tensor, mask : torch.Tensor) -> Tuple[torch.Tensor, 
                                                                                  torch.Tensor,
-                                                                                 torch.Tensor, 
+                                                                                 torch.Tensor,       
                                                                                  torch.Tensor, ]: 
-        """_summary_
+        """used by train_step and evaluate
 
         Args:
             phy (torch.Tensor): _description_

phyloencode/cli/cmd_encode.py
@@ -75,7 +75,8 @@ def main ():
                 # if labels are present, save them to a csv file
                 # check if labels are in the correct format
                 labels = f['labels'][...]
-                label_names = [lbl.decode('utf-8') if isinstance(lbl, bytes) else lbl for lbl in f['label_names'][...][0]]
+                label_names = [lbl.decode('utf-8') if isinstance(lbl, bytes) 
+                               else lbl for lbl in f['label_names'][...][0]]
                 df = pd.DataFrame(labels, columns=label_names)
                 df.to_csv(out_file_prefix + ".labels.csv", index = False)
             else:
@@ -83,8 +84,10 @@ def main ():
                 # raise ValueError("No labels found in the hdf5 file.")
 
     elif tree_data_fn.endswith('.csv') or tree_data_fn.endswith(".cblv"):
-        test_phy_data = pd.read_csv(tree_data_fn, header = None, index_col = None).to_numpy(dtype=np.float32)
-        test_aux_data = pd.read_csv(aux_data_fn, header = None, index_col = None).to_numpy(dtype=np.float32)
+        test_phy_data = pd.read_csv(tree_data_fn, header = None, 
+                                    index_col = None).to_numpy(dtype=np.float32)
+        test_aux_data = pd.read_csv(aux_data_fn, header = None, 
+                                    index_col = None).to_numpy(dtype=np.float32)
         test_phy_data = torch.tensor(test_phy_data, dtype = torch.float32)
         test_aux_data = torch.tensor(test_aux_data, dtype = torch.float32)
     else:
@@ -99,6 +102,8 @@ def main ():
         test_phy_data = test_phy_data.reshape(-1, max_tips, nc)[:, :, :num_channels].flatten(start_dim=1)
         # print(test_phy_data[0,0:8])
 
+    print(test_phy_data.shape)
+    
     # make predictions with trained model
     tree_autoencoder = PhyloAutoencoder(model = ae_model, optimizer = torch.optim.Adam)
 

phyloencode/cli/cmd_train.py
@@ -33,9 +33,6 @@ def main():
         config = ph.utils.read_config(args.config)
         update_settings_from_config(settings = settings, config = config)
 
-    # Optimizer settings
-    lr = settings['learning_rate']
-    wd = settings['weight_decay']
 
     # required command line argument
     data_fn     = args.trn_data
@@ -50,6 +47,7 @@ def main():
 
         # num tips is important for masking. 
         # TODO: Check a potential discrepency in num_taxa with phyddle --format output
+        # TODO: maybe move some of this to backend: the Data object from DataProcessors should maybe handle all this
             
         phy_data_np = np.array(f['phy_data'][0:ns,...], dtype = np.float32)
         # extract the specified subset of channels ("num_channels")
@@ -84,6 +82,8 @@ def main():
                                                     num_workers = settings["num_workers"])
         
     # create model
+    # TODO: The trainer is an object that mutates a model, maybe it makes the most sense for the trainer to be an attribute of the model?
+    # TODO: model should know which aux variable is num tips???
     ae_model    = AECNN(
                         num_structured_input_channel  = ae_data.num_channels, 
                         structured_input_width        = ae_data.phy_width,
@@ -99,6 +99,10 @@ def main():
                         )
     
     # optimizer
+    # settings
+    lr = settings['learning_rate']
+    wd = settings['weight_decay']
+
     # opt = AdamW(ae_model.parameters(), lr=lr, weight_decay=wd)
     opt         = AdamW(split_params_by_wd(ae_model, wd), lr=lr)
     lr_schedlr  = torch.optim.lr_scheduler.OneCycleLR(
@@ -110,22 +114,24 @@ def main():
                         anneal_strategy='cos',
                         cycle_momentum=False
                         )
-    # Loss objects
+    
+    # Loss objects (stateful)    
+    # TODO: remove device from RBF class and PhyLoss, I dont think  I need it.
+        # loss weights
     loss_weights = (settings["phy_loss_weight"], 
                     settings["char_weight"], 
                     1 - settings["phy_loss_weight"], 
                     settings["mmd_lambda"], 
                     settings["vz_lambda"])
-    
-    # TODO: remove device from RBF class and PhyLoss, I dont think  I need it.
+
     train_loss  = PhyLoss(loss_weights, ae_data.ntax_cidx, ae_model.char_type,
                                ae_model.latent_layer_type, "cuda")
     val_loss    = PhyLoss(loss_weights, ae_data.ntax_cidx,  ae_model.char_type,
                                ae_model.latent_layer_type, "cuda", validation = True)
 
 
-    # create Trainer
-    # TODO: should be ae_trainer, once some of its methods are moved to the model class where they belong
+    # create model trainer and trained model container
+    # the model, the data, and the loss come together here
     tree_ae     = PhyloAutoencoder(
                         model           = ae_model, 
                         optimizer       = opt, 
@@ -141,7 +147,7 @@ def main():
     #######################################
     # create and add data loaders to trainer and train model
 
-    tree_ae.set_data_loaders(train_loader=trn_loader, val_loader=val_loader)
+    tree_ae.set_data_loaders(train_loader=trn_loader, val_loader=val_loader) 
     tree_ae.train(num_epochs = settings["num_epochs"], seed = settings["seed"])
     if tree_ae.track_grad:
         plot_gradient_norms(tree_ae.mean_layer_grad_norm, 
@@ -172,7 +178,7 @@ def main():
     # save true values of test data in cblv format
     phy_true_df = pd.DataFrame(test_phy_data.numpy())
     aux_true_df = pd.DataFrame(test_aux_data.numpy())
-    phy_true_df.to_csv(settings["out_prefix"] + ".phy_true.cblv", 
+    phy_true_df.to_csv(settings["out_prefix"] + ".phy_true.cblv.csv", 
                        header = False, index = False)
     aux_true_df.to_csv(settings["out_prefix"] + ".aux_true.csv", 
                        header = False, index = False)
@@ -193,17 +199,18 @@ def main():
     latent_testdat_df.to_csv(settings["out_prefix"] + ".testdat_latent.csv", 
                              header = False, index = False)
 
-    phy_pred, auxpred = tree_ae.predict(phydat, auxdat)
+    phy_pred, aux_pred = tree_ae.predict(phydat, auxdat)
+    # phy_pred, char_pred, aux_pred, latent_pred = tree_ae.predict(phydat, auxdat)
 
     # transform and flatten predicted data
     phy_pred = phy_normalizer.inverse_transform(phy_pred.reshape((phy_pred.shape[0], -1), 
                                                                  order = "F"))
-    auxpred  = aux_normalizer.inverse_transform(auxpred)
+    aux_pred  = aux_normalizer.inverse_transform(aux_pred)
 
     # save predictions to file
     phy_pred_df = pd.DataFrame(phy_pred)
-    aux_pred_df = pd.DataFrame(auxpred)
-    phy_pred_df.to_csv(settings["out_prefix"] + ".phy_pred.cblv", 
+    aux_pred_df = pd.DataFrame(aux_pred)
+    phy_pred_df.to_csv(settings["out_prefix"] + ".phy_pred.cblv.csv", 
                        header = False, index = False)
     aux_pred_df.to_csv(settings["out_prefix"] + ".aux_pred.csv", 
                        header  = False, index = False)